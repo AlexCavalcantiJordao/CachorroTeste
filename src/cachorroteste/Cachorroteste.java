733d20c64cdb22a4717abbc84155b6be94635182
AlexCavalcantiJordao <alex37705732@gmail.com>
2023-07-03 14:29:35 -0300

    build/ build.xml manifest.mf nbproject/ src/ test/

---
+++ src/cachorroteste/Cachorroteste.java
@@ -0,0 +1,13 @@
+package cachorroteste;
+
+public class Cachorroteste {
+
+    public static void main(String[] args) {
+
+        cachorro beagle = new cachorro();
+        beagle.raca = "beagle";
+        beagle.tamanho = 40;
+        beagle.latir();
+
+    }
+}
---
+++ src/cachorroteste/cachorro.java
@@ -0,0 +1,11 @@
+package cachorroteste;
+
+public class cachorro {
+
+    int tamanho;
+    String raca;
+
+    void latir() {
+        System.out.println("au, au, au, au");
+    }
+}
